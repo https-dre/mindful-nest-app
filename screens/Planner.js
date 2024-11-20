a39ccdaa1022874420f63dc261928ab37d65ea45
http.dre <diaso.andre@outlook.com>
2024-11-20 19:02:16 -0300

    adiciona o Planner, ainda não finalizado

---
+++ screens/Planner.js
@@ -0,0 +1,99 @@
+import { SafeAreaView, Text, View, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useNavigation } from "@react-navigation/native";
+
+import { useState } from "react";
+
+import { TasksTab } from "./PlannerTabs/TasksTab";
+import { ProjectsTab } from "./PlannerTabs/ProjectsTab";
+
+const Stack2 = createNativeStackNavigator()
+
+export const Planner = () => {
+    const navigation = useNavigation();
+
+    const [tasks, setTasks] = useState(null)
+    const [yourProjects, setYourProjects] = useState(null)
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+            <View  style={styles.navBar}>
+                <View style={styles.navMenu}>
+                    <TouchableOpacity 
+                        style={styles.navButton}
+                        onPress={()=> {
+                            navigation.navigate("Tasks")
+                            setTasks(styles.selected)
+                            setYourProjects(null)
+                        }}
+                    >
+                        <Text>Tasks Para Hoje</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        style={styles.navButton}
+                        onPress={()=> {
+                            navigation.navigate("Projects")
+                            setTasks(null)
+                            setYourProjects(styles.selected)
+                        }}
+                    >
+                        <Text>Todos os Projetos</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.bottomBar}>
+                    <View style={[tasks, {width:"50%"}]}></View>
+                    <View style={[yourProjects, {width:"50%"}]}></View>
+                </View>
+            </View>
+            <View style={{flex: 1, marginTop: 5}}>
+                <Stack2.Navigator 
+                    initialRouteName="Tasks"
+                    screenOptions={{ headerShown: false }}
+                >
+                    <Stack2.Screen 
+                        name="Tasks" 
+                        component={TasksTab}
+                    />
+                    <Stack2.Screen 
+                        name="Projects" 
+                        component={ProjectsTab} 
+                    />
+                </Stack2.Navigator>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    navButton: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "50%",
+        backgroundColor: 'transparent'
+    },
+    navBar: {
+        paddingTop: 10,
+        marginHorizontal: 15,
+        justifyContent: "center",
+        backgroundColor: "white",
+        margin: 0
+    },
+    navMenu: {
+        justifyContent: "center",
+        flexDirection: "row",
+        backgroundColor: "transparent"
+    },
+    selected: {
+        padding: 1,
+        backgroundColor: "#007AFF",
+        width: "50%",
+        borderRadius: 10
+    },
+    bottomBar: {
+        width: "100%",
+        backgroundColor: "#E4E4E4",
+        padding: 1,
+        margin: 0
+    }
+})
