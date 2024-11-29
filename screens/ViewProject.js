41ff097f77c9388389b7601eb95ce8374e68421e
http.dre <diaso.andre@outlook.com>
2024-11-29 19:08:40 -0300

    começa tela para mostrar informações sobre um projeto.

---
+++ screens/ViewProject.js
@@ -0,0 +1,10 @@
+import { SafeAreaView, Text, View, 
+    TouchableOpacity, StyleSheet, ScrollView } from "react-native";
+
+export const ViewProject = () => {
+    return (
+        <SafeAreaView>
+            <Text>Ver Projeto</Text>
+        </SafeAreaView>
+    )
+}
