c3dfb53c82c3470607bc2356f4f24150fd632050
http.dre <136815797+https-dre@users.noreply.github.com>
2024-12-02 12:06:44 +0000

    fix: corrige bug com o Carrossel
    O ScrollView estava captando o evento de toque do usuário, o que impossibilitava
    a interação com o Carrossel

--- screens/Home.js
+++ screens/Home.js
@@ -1,66 +1,67 @@
-import { KeyboardAvoidingView, TouchableWithoutFeedback, SafeAreaView, StyleSheet,
-View, Text, TextInput, Image, Platform, Keyboard, TouchableOpacity, ScrollView } from "react-native";
+import {
+  TouchableWithoutFeedback, SafeAreaView, StyleSheet,
+  View, Text, TextInput, Image, Platform, Keyboard, TouchableOpacity, ScrollView
+} from "react-native";
 
 import Icon from "react-native-vector-icons/Ionicons";
 
 import Carousel from "../components/Carousel";
 import { projects } from "../exampledata";
 
-export const Home = ({navigation}) => {
+export const Home = () => {
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <SafeAreaView style={styles.container}>
-        <ScrollView>
-          { /* header */ }
-          <View style={styles.header}>
-            <View style={{flexDirection: "row", alignItems: "center", gap: 10}}>
-              <Image source={require("../assets/foto-perfil-exemplo.jpg")} />
-              <View>
-                <Text style={{fontSize: 20, color: "#222B45"}}>André Dias</Text>
-                <Text style={{color: "#88929E"}}>diaso.andre@outlook.com</Text>
-              </View>
-            </View>
-            
-            <View style={{flexDirection: "row", gap: 10, justifyContent: "flex-end"}}>
-              <TouchableOpacity style={styles.elipseButton}>
-                <Icon name="document-text-outline" color="#363538" size={20}/>
-              </TouchableOpacity>
 
-              <TouchableOpacity style={styles.elipseButton}>
-                <Icon name="notifications-outline" color="#363538" size={20}/>
-              </TouchableOpacity>
+        { /* header */}
+        <View style={styles.header}>
+          <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+            <Image source={require("../assets/foto-perfil-exemplo.jpg")} />
+            <View>
+              <Text style={{ fontSize: 20, color: "#222B45" }}>André Dias</Text>
+              <Text style={{ color: "#88929E" }}>diaso.andre@outlook.com</Text>
             </View>
           </View>
 
+          <View style={{ flexDirection: "row", gap: 10, justifyContent: "flex-end" }}>
+            <TouchableOpacity style={styles.elipseButton}>
+              <Icon name="document-text-outline" color="#363538" size={20} />
+            </TouchableOpacity>
 
-          { /* TextBox "Encontre suas Tasks e Projetos" */ }
-          <View style={styles.textBoxView}>
-            <View style={{justifyContent: "center", paddingHorizontal: 10}}>
-              <Icon name="search" color="#8F9BB3" size={20}/>
-            </View>
-            <TextInput placeholder="Encontre suas Tasks e Projetos" style={{width: "100%"}} placeholderTextColor="#8F9BB3" />
+            <TouchableOpacity style={styles.elipseButton}>
+              <Icon name="notifications-outline" color="#363538" size={20} />
+            </TouchableOpacity>
           </View>
+        </View>
 
 
-          { /* Lista de Projetos */ }
-          <View style={{marginBottom: 20}}>
-            <View style={{flexDirection: "row", justifyContent: "space-between", paddingHorizontal: 20}}>
-              <View>
-                <Text style={{fontFamily: "SpaceGroteskMedium", fontSize: 18}}>Projetos</Text>
-                <Text>Você tem {projects.length} projetos em Progresso!</Text>
-              </View>
-
-              <TouchableOpacity style={styles.blueButtom}>
-                <Icon name="add" size={20} color="#007AFF" />
-                <Text style={{color: "#007AFF"}}>Criar</Text>
-              </TouchableOpacity>
-            </View>
+        { /* TextBox "Encontre suas Tasks e Projetos" */}
+        <View style={styles.textBoxView}>
+          <View style={{ justifyContent: "center", paddingHorizontal: 10 }}>
+            <Icon name="search" color="#8F9BB3" size={20} />
           </View>
+          <TextInput placeholder="Encontre suas Tasks e Projetos" style={{ width: "100%" }} placeholderTextColor="#8F9BB3" />
+        </View>
 
-         { /* Carousel */}
-          <Carousel data={projects}/>
 
-        </ScrollView>
+        { /* Lista de Projetos */}
+        <View style={{ marginBottom: 20 }}>
+          <View style={{ flexDirection: "row", justifyContent: "space-between", paddingHorizontal: 20 }}>
+            <View>
+              <Text style={{ fontFamily: "SpaceGroteskMedium", fontSize: 18 }}>Projetos</Text>
+              <Text>Você tem {projects.length} projetos em Progresso!</Text>
+            </View>
+
+            <TouchableOpacity style={styles.blueButtom}>
+              <Icon name="add" size={20} color="#007AFF" />
+              <Text style={{ color: "#007AFF" }}>Criar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        { /* Carousel */}
+        <Carousel data={projects} />
+
       </SafeAreaView>
     </TouchableWithoutFeedback>
   )
