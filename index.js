94492c23d54ae4543cf56df8f2c30b06aa3b09c4
http.dre <diaso.andre@outlook.com>
2024-12-02 18:35:53 -0300

    fix: adiciona o arquivo index.js, que é responsável por rodar o projeto na forma que o expo cli cria

---
+++ index.js
@@ -0,0 +1,5 @@
+import { registerRootComponent } from 'expo';  // Importa o método do Expo
+import App from './App';  // Caminho para o seu componente principal App.js
+
+
+registerRootComponent(App);
