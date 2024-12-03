044fe301f3b688e84251d01bc2a27daa078aee65
Leonardo8896 <pkafonso8896@gmail.com>
2024-12-03 19:21:22 -0300

    Adição de campos e opção de adicionar funcional

--- components/BoxCreateTask.js
+++ components/BoxCreateTask.js
@@ -3,20 +3,25 @@ import { TouchableOpacity, Text, View, StyleSheet, Animated, TextInput } from "r
 import {Picker} from '@react-native-picker/picker';
 import Icon from 'react-native-vector-icons/FontAwesome6';
 import { projects } from "../exampledata";
-import DatePicker from "react-native-date-picker";
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 
 export const BoxCreateTask = ({
     extVisible,
     onClose,
-    style
+    style,
+    taskHold
 }) => {
     const [visible, setVisible] = useState(extVisible)
     const [heightFull, setHeightFull] = useState(false)
     const [projectsAll, setProjectsAll] = useState(null)
     const [selectedProject, setSelectedProject] = useState(null)
     const [date, setDate] = useState(new Date())
+    const [time, setTime] = useState(new Date())
     const [open, setOpen] = useState(false)
+    const [openTime, setOpenTime] = useState(false)
+    const [name, setName] = useState(null)
+    const [status, setStatus] = useState(0)
     // const [hook, setHook] = useState(true)
 
     const fadeAnim = useRef(new Animated.Value(0)).current;
@@ -75,6 +80,50 @@ export const BoxCreateTask = ({
     //     }
     // }, [visible, withButton]);
 
+    const onChangeDate = (event, selectedDate) => {
+        if (event.type === "set") { // Verifica se foi confirmado
+          const currentDate = selectedDate || date;
+          setDate(currentDate);
+        }
+        setOpen(false); // Fecha o seletor em qualquer caso
+    };
+
+    const onChangeTime = (event, selectedTime) => {
+        if (event.type === "set") { // Verifica se foi confirmado
+          const currentTime = selectedTime || time;
+          setTime(currentTime);
+        }
+        setOpenTime(false); // Fecha o seletor em qualquer caso
+    };
+
+    const taskSave = () => {
+        if (!selectedProject) {
+            console.error("Projeto não selecionado");
+            return;
+        }
+    
+        const data = {
+            task: {
+                name: name,
+                date: new Date(
+                    date.getFullYear(),
+                    date.getMonth(),
+                    date.getDate(),
+                    time.getHours(),
+                    time.getMinutes()
+                ).toISOString(),
+                status: status,
+            },
+            project: {
+                name: selectedProject
+            },
+        };
+    
+        taskHold(data);
+    };
+    
+      
+
     return (
         <Animated.View style={[{marginTop: (height/2)*(-1), opacity: fadeAnim, height: heightFull? "auto":0}, styles.mainView, styles.style]}>
             <View style={[styles.headerView]}>
@@ -90,7 +139,10 @@ export const BoxCreateTask = ({
             </View>
             <View style={[styles.contentView]}>
                 <Text style={{fontSize: 18, color: "blue"}}>Nome</Text>
-                <TextInput style={{backgroundColor: "white", width: "80%", height: 40, borderRadius: 20, alignItems: "center"}}/>
+                <TextInput 
+                    style={{backgroundColor: "white", width: "80%", height: 40, borderRadius: 10, alignItems: "center", fontSize: 16}}
+                    onChangeText={(text)=>{setName(text)}}
+                />
                 <Text style={{fontSize: 18, color: "blue"}}>Projeto</Text>
                 <View style={styles.pickerWraper}>
                     <Picker
@@ -106,7 +158,7 @@ export const BoxCreateTask = ({
                     <Picker
                         selectedValue={selectedProject}
                         style={styles.projectsPicker}
-                        onValueChange={(itemValue) => setSelectedProject(itemValue)}
+                        onValueChange={(itemValue) => setStatus(itemValue)}
                     >
                         <Picker.Item label="Aberto" value={0} style={styles.pPickerItem}/>
                         <Picker.Item label="Em andamento" value={1} style={styles.pPickerItem}/>
@@ -114,25 +166,51 @@ export const BoxCreateTask = ({
                         <Picker.Item label="Arquivada" value={3} style={styles.pPickerItem}/>
                     </Picker>
                 </View>
-                <TouchableOpacity onPress={()=>{setOpen(true)}}>
-                    <Text>{date.toLocaleDateString()}</Text>
+                <Text style={{fontSize: 18, color: "blue"}}>Data</Text>
+                <TouchableOpacity onPress={()=>{setOpen(true)}} style={styles.dateInput}>
+                    <Text style={{fontSize: 18}}>{date.toLocaleDateString(
+                        "pt-BR",
+                        {
+                            day: "2-digit",
+                            month: "2-digit",
+                            year: "numeric"
+                        }
+                    )}</Text>
                 </TouchableOpacity>
-                <DatePicker
-                    modal
-                    open={open}
-                    date={date}
-                    onConfirm={(selectedDate)=>{
-                        setOpen(false)
-                        setDate(selectedDate)
-                    }}
-                    onCancel={()=>{
-                        setOpen(false)
-                    }}
-                />
+                {
+                    open && <DateTimePicker
+                        value={date}
+                        mode="date"
+                        display="default"
+                        onChange={onChangeDate}
+                    />
+                }
+                <Text style={{fontSize: 18, color: "blue"}}>Horário</Text>
+                <TouchableOpacity onPress={()=>{setOpenTime(true)}} style={styles.dateInput}>
+                    <Text style={{fontSize: 18}}>{date.toLocaleTimeString(
+                        "pt-BR",
+                        {
+                            hour: "2-digit",
+                            minute: "2-digit",
+                            dayPeriod: "short",
+                            hour12: true
+                        }
+                    )}</Text>
+                </TouchableOpacity>
+                {
+                    openTime && <DateTimePicker
+                        value={date}
+                        mode="time"
+                        display="default"
+                        onChange={onChangeTime}
+                    />
+                }
+
             </View>
             <TouchableOpacity 
                 style={[{display: "flex"}, styles.footerView]}
                 onPress={()=>{
+                    taskSave()
                     setVisible(false)
                     fadeOut()
                 }}
@@ -147,9 +225,8 @@ export const BoxCreateTask = ({
 const styles = StyleSheet.create({
     mainView: {
         position: "absolute",
-        top: "50%",
-        left: "10%",
-        right: "10%",
+        top: "20%",
+        left: "15%",
         width: "80%",
         borderRadius: 10,
         zIndex: 3,
@@ -193,5 +270,13 @@ const styles = StyleSheet.create({
     pPickerItem: {
         flex: 1
 
+    },
+    dateInput: {
+        height: 40,
+        backgroundColor: "white",
+        width: "80%",
+        alignItems: "center",
+        textAlign: "center",
+        borderRadius: 10
     }
 })
