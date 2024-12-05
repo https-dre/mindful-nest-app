86d02073ffd80cf3aeb1ab6e87be4c7902c2a2a5
http.dre <diaso.andre@outlook.com>
2024-12-05 19:24:14 -0300

    fix: corrige erros com a estilização da tela de configurações do usuário

--- screens/UserConfig.js
+++ screens/UserConfig.js
@@ -1,57 +1,43 @@
 import { Text, SafeAreaView, StyleSheet,TouchableOpacity,Image,View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export const UserConfig = () => {
   return (
-    <SafeAreaView>
-      <View style={styles.userConteiner}>
+    <SafeAreaView style={{flex: 1, backgroundColor: "white"}}>
+      <View style={{ borderWidth: 1, width: "90%", alignSelf: "center", borderColor: "#CACACA", borderRadius: 15, marginTop: 50}}>
         <View style={styles.userDataConteiner}>
-          <Image  style={styles.espace} source={require("../assets/foto-perfil-exemplo.jpg")} />
-          <View style={styles.espace}>
-            <Text>André Dias</Text>
-            <Text style={{color: "#888888"}}>diaso.andre@outlook.com</Text>
+          <Image source={require("../assets/user-square-example.jpg")} />
+          <View>
+            <Text style={{ fontWeight: "bold", fontSize: 16}}>André Dias</Text>
+            <Text style={{color: "gray"}}>diaso.andre@outlook.com</Text>
           </View>
+          <Ionicons name="arrow-down-circle-outline" size={20} />
         </View>
         <View style={styles.buttonConteiner}>
           <TouchableOpacity style={styles.button}>
-            <AntDesign name="user" size={24} color="black" />
-            <Text> Conta</Text>
+            <AntDesign name="user" size={25} />
+            <Text style={{fontSize: 16, fontWeight: "bold"}}>Profile</Text>
           </TouchableOpacity>
+
           <TouchableOpacity style={styles.button}>
-            <FontAwesome name="cogs" size={24} color="black" />
-            <Text> Configurações</Text>
+            <Ionicons name="wallet-outline" size={25} color={"#5F0FC6"}/>
+            <Text style={{fontSize: 16, fontWeight: "bold", color: "#5F0FC6"}}>Upgrade Plan</Text>
           </TouchableOpacity>
+
           <TouchableOpacity style={styles.button}>
-            <AntDesign name="creditcard" size={24} color="#5f0fc6" />
-            <Text style={{color: "#5914c2"}}> A</Text>
-            <Text style={{color: "#5319bd"}}>t</Text>
-            <Text style={{color: "#4d1eb9"}}>u</Text>
-            <Text style={{color: "#4722b4"}}>a</Text>
-            <Text style={{color: "#4127b0"}}>l</Text>
-            <Text style={{color: "#3b2cab"}}>i</Text>
-            <Text style={{color: "#3531a7"}}>z</Text>
-            <Text style={{color: "#2f36a2"}}>a</Text>
-            <Text style={{color: "#293b9e"}}>r </Text>
-            <Text style={{color: "#234099"}}>p</Text>
-            <Text style={{color: "#1d4495"}}>l</Text>
-            <Text style={{color: "#174990"}}>a</Text>
-            <Text style={{color: "#114e8c"}}>n</Text>
-            <Text style={{color: "#114e8c"}}>o</Text>
+            <Ionicons name="settings-outline" size={25} />
+            <Text style={{fontSize: 16, fontWeight: "bold"}}>Settings</Text>
           </TouchableOpacity>
-          <View
-            style={{
-              borderBottomColor: 'black',
-              borderBottomWidth: 1,
-            }}
-          ></View>
+
           <TouchableOpacity style={styles.button}>
-            <AntDesign name="adduser" size={24} color="black" />
-            <Text> Adicionar conta</Text>
+            <Ionicons name="person-add-outline" size={25} />
+            <Text style={{fontSize: 16, fontWeight: "bold"}}>Add Account</Text>
           </TouchableOpacity>
+
           <TouchableOpacity style={styles.button}>
-            <AntDesign name="logout" size={24} color="black" />
-            <Text> Sair</Text>
+            <Ionicons name="log-out-outline" size={25} />
+            <Text style={{fontSize: 16, fontWeight: "bold"}}>Logout</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -61,39 +47,24 @@ export const UserConfig = () => {
 
 const styles = StyleSheet.create({
   userDataConteiner: {
-    flex: 1,
+    margin: 15,
     flexDirection: "row",
-    marginLeft: "5%",
-    marginRight: "5%",
-    marginTop: "5%",
-    padding: "5%",
-    alignContent: "center",
-    backgroundColor: "white",
-    borderRadius: 45,
-  },
-
-  userConteiner: {
-    marginLeft: "10%",
-    marginRight: "10%",
-    marginTop: "30%",
-    borderWidth: "1px",
-    borderRadius: 45,
-    padding: "1%"
+    gap: 10,
+    justifyContent: "space-around",
+    alignItems: "center"
   },
   buttonConteiner: {
-    marginLeft: "5%",
-    marginRight: "5%",
-    marginTop: "5%",
-    marginBottom: "5%",
+    margin: 10,
+    alignSelf: "center",
+    width: "90%",
     backgroundColor: "#FAFAFA",
-    padding: "10%",
-    borderRadius: 45,
+    borderRadius: 10,
   },
   button: {
-    flex: 1,
     flexDirection: "row",
-    marginTop: "5%",
-    marginBottom: "5%",
+    alignItems: "center",
+    gap: 20,
+    padding: 20
   },
   espace: {
     marginLeft: "1%",
