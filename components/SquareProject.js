1b2796a3b9545ac0edd1f8d16e180fd2d41d95e5
http.dre <diaso.andre@outlook.com>
2024-12-10 00:00:44 -0300

    adiciona o cálculo de progresso ao componente "SquareProject"

--- components/SquareProject.js
+++ components/SquareProject.js
@@ -1,10 +1,13 @@
 import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/Ionicons";
+import { useAppState } from "../AppStateContext";
+import { useEffect } from "react";
 
 export const SquareProject = ({ size = 150, data }) => {
     let participantesImage = []
     const navigation = useNavigation();
+    const { projects, setProjects, tasks } = useAppState();
 
     for (const index in data.users) {
         data.users.map(user => {
@@ -15,8 +18,29 @@ export const SquareProject = ({ size = 150, data }) => {
     }
 
     function handleNavigation() {
-        navigation.navigate("ViewProject", { projectData: data })
+        navigation.navigate("ViewProject", {projectData: data})
     }
+
+    const currentProjectIndex = projects.findIndex(p => p.id === data.id);
+
+    const getTasksByProjectIndex = (p_index) => {
+        return projects[p_index].tasks.map(id => {
+        return tasks[tasks.findIndex(t => t.id === id)];
+        });
+    }
+
+    useEffect(() => {
+        const tasksFromCurrentProject = getTasksByProjectIndex(currentProjectIndex);
+        let finishedTasksCount = 0;
+        tasksFromCurrentProject.forEach(t => t.status >= 2 ? finishedTasksCount += 1 : 0);
+        const projectProgress = (finishedTasksCount / tasksFromCurrentProject.length) * 100
+        const progressFormated = `${projectProgress}%`
+        setProjects(prev => prev.map(p => p.id === data.id ? { ...p, progress: projectProgress } : p));
+    }, [tasks]);
+
+    const formatedProgress = () => {
+		return `${parseInt(projects[currentProjectIndex].progress)}%`
+	}
 
     return (
         <View style={[styles.container, {backgroundColor: data.backColor, width: size, minHeight: size}]}>
@@ -33,12 +57,12 @@ export const SquareProject = ({ size = 150, data }) => {
             <View style={{ alignSelf: "center", width: "100%"}}>
                 <View style={{ flexDirection: "row", justifyContent: "space-between", marginBottom: 5}}>
                     <Text style={{ color: "#EBEBEB" }}>Progresso</Text>
-                    <Text style={{ color: "#EBEBEB" }}>{data.progress}</Text>
+                    <Text style={{ color: "#EBEBEB" }}>{formatedProgress()}</Text>
                 </View>
 
                 { /* Barra de Progresso */}
                 <View style={{ opacity: 0.21, backgroundColor: "black", alignSelf: "center", height: 1, width: "100%" }}>
-                    <View style={{ height: 1, width: data.progress, backgroundColor: "white", opacity: 1 }} />
+                    <View style={{ height: 1, width: formatedProgress(), backgroundColor: "white", opacity: 1 }} />
                 </View>
             </View>
 
