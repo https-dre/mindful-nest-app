666353b8cc6108922c6f98412ca7019c9a0df24e
http.dre <diaso.andre@outlook.com>
2024-12-10 11:22:29 -0300

    adiciona nova função para formatar datas em barras "dd/mm/aaaa"

--- utilities.js
+++ utilities.js
@@ -43,3 +43,11 @@ export function formatHours(date) {
 		minute: '2-digit',
 	});
 }
+
+export function formatPadDate(date) {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Janeiro é 0
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+}
