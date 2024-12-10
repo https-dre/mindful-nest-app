446796d342eabd7ce1d6f13f364979e99b6dc5d7
http.dre <diaso.andre@outlook.com>
2024-12-10 14:03:55 -0300

    fix: corrige bugs "VirtualizedLists should never be nested" com a tela para de escolha de planos

--- screens/Login/ChoosePlan.js
+++ screens/Login/ChoosePlan.js
@@ -1,16 +1,15 @@
-import { SafeAreaView, View, Text, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
-
+import { SafeAreaView, Text, StyleSheet, FlatList } from "react-native";
 import { Plan } from "../../components/Plan";
-import BasicLoginStyle from "./BasicLoginStyles"
+import BasicLoginStyle from "./BasicLoginStyles";
 
 export const ChoosePlan = () => {
   const data = [
-                '10 GB de anotações',
-                '7 GB de registros no Planner',
-                'Acesso total a Comunidade',
-                'Compartilhe seus projetos com mais de duas pessoas',
-                'Acesso antecipado a funcionalidades novas',
-              ]
+    '10 GB de anotações',
+    '7 GB de registros no Planner',
+    'Acesso total a Comunidade',
+    'Compartilhe seus projetos com mais de duas pessoas',
+    'Acesso antecipado a funcionalidades novas',
+  ];
 
   const freePlanData = [
     "1 GB de anotações",
@@ -18,19 +17,29 @@ export const ChoosePlan = () => {
     "Acesso à comunidade limitado",
     "Recursos Limitados",
     "Funcionalidades Básicas"
-  ]
+  ];
+
+  const renderItem = ({ item }) => (
+    <Plan data={item.data} price={item.price} name={item.name} repeat={item.repeat} desconto={item.desconto} />
+  );
+
+  const plans = [
+    { data, price: 25.00, name: "Premium", repeat: "Mensal", desconto: 20 },
+    { data: freePlanData, price: 0, name: "Gratuito", hasMessage: false }
+  ];
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={[BasicLoginStyle.titleSpaceGrotesk, {margin: 20, maxWidth: 250, fontSize: 40}]}>Escolha um Plano</Text>
 
-      <ScrollView style={{alignSelf: "center"}}>
-        <Plan data={data} price={25.00} name="Premium" repeat="Mensal" desconto={20} />
-        <Plan data={freePlanData} price={0} name="Gratuito" hasMessage={false}/>
-      </ScrollView>
-
+      <FlatList
+        data={plans}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.name}
+      />
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -38,4 +47,4 @@ const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: "white"
   }
-})
+});
